3f0021e50e1b63500246667f376fadaeca015379
Boyeon Ihn <boyeonihn@gmail.com>
2023-10-02 04:38:17 +0900

    feat: create default themes via styled-comp

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import App from './App.tsx';
+import { darkTheme, lightTheme } from './theme.ts';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  colors: {
+    main: '#408EC6',
+    secondary: '#1E2761',
+    accent: '#7A2048',
+  },
+};
+
+export const darkTheme: DefaultTheme = {
+  colors: {
+    accent: '#408EC6',
+    secondary: '#1E2761',
+    main: '#7A2048',
+  },
+};

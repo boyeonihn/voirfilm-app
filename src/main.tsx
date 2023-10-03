b1f9ddf7936db54d19df416b71b7855bad438d0e
Boyeon Ihn <boyeonihn@gmail.com>
2023-10-03 20:14:50 +0900

    feat: implement react-query for data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'styled-components';
 import { RouterProvider } from 'react-router-dom';
 import { myTheme } from './theme.ts';
 import './index.css';
 import { router } from './router.tsx';
 
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={myTheme}>
         <RouterProvider router={router} />
       </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );

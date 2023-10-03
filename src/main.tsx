71bcffb39fd7bd0c77faa8ecaf2b858074b93f76
Boyeon Ihn <boyeonihn@gmail.com>
2023-10-03 20:14:15 +0900

    feat: implement routerProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import App from './App.tsx';
+import { RouterProvider } from 'react-router-dom';
 import { darkTheme, lightTheme } from './theme.ts';
 import './index.css';
+import { router } from './router.tsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+        <RouterProvider router={router} />
   </React.StrictMode>
 );

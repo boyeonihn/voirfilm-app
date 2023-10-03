c75302093c70f5df76a026bb07eba52808b38f29
Boyeon Ihn <boyeonihn@gmail.com>
2023-10-03 20:12:04 +0900

    style: update theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,17 +1,9 @@
 import { DefaultTheme } from 'styled-components';
 
-export const lightTheme: DefaultTheme = {
+export const myTheme: DefaultTheme = {
   colors: {
     main: '#408EC6',
     secondary: '#1E2761',
     accent: '#7A2048',
-  },
-};
-
-export const darkTheme: DefaultTheme = {
-  colors: {
-    accent: '#408EC6',
-    secondary: '#1E2761',
-    main: '#7A2048',
   },
 };

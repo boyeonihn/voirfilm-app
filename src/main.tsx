6abbbc50e0fa5f6d012b06c3257cb11667f17b41
Boyeon Ihn <boyeonihn@gmail.com>
2023-10-03 20:14:36 +0900

    chore: update name of imported Theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { RouterProvider } from 'react-router-dom';
-import { darkTheme, lightTheme } from './theme.ts';
+import { myTheme } from './theme.ts';
 import './index.css';
 import { router } from './router.tsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+      <ThemeProvider theme={myTheme}>
         <RouterProvider router={router} />
+      </ThemeProvider>
   </React.StrictMode>
 );

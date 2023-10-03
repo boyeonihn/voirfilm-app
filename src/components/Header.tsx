5406729b4c9da080b241b679f4297da253e6e4a2
Boyeon Ihn <boyeonihn@gmail.com>
2023-10-03 20:12:50 +0900

    feat: create Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,40 @@
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Col = styled.div``;
+const Logo = styled.svg`
+  margin-right: 50px;
+`;
+
+const Links = styled.ul`
+  display: flex;
+  li {
+    text-transform: uppercase;
+    font-weight: bold;
+  }
+`;
+
+const LinkItem = styled.li``;
+
 export const Header = () => {
-  return <header>VOIRFILM</header>;
+  return (
+    <Nav>
+      <Col>
+        <Logo />
+        <Links>
+          <LinkItem>Popular</LinkItem>
+          <LinkItem>Coming Soon</LinkItem>
+          <LinkItem>Now Playing</LinkItem>
+        </Links>
+      </Col>
+      <Col>
+        <button>Search</button>
+      </Col>
+    </Nav>
+  );
 };

876294e02e84ff058a5b68b75f5cb8829ce12cdd
Boyeon Ihn <boyeonihn@gmail.com>
2023-10-06 00:55:04 +0900

    style: create styled-components for Layout

--- src/pages/Layout/styled.ts
+++ src/pages/Layout/styled.ts
@@ -19,3 +19,57 @@ export const Overlay = styled(motion.div)`
   justify-content: center;
   align-items: center;
 `;
+
+export const ModalCover = styled.img`
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+  width: 100%;
+  height: 350px;
+`;
+
+export const ModalInfo = styled.div`
+  position: relative;
+  top: -115px;
+  padding: 20px;
+`;
+
+export const ModalTitle = styled.h2`
+  text-align: left;
+  font-size: 3rem;
+  font-family: 'Berkshire Swash', cursive;
+  font-weight: bold;
+  color: white;
+  text-shadow:
+    3px 3px 0 #131314,
+    -1px -1px 0 #131314,
+    1px -1px 0 #131314,
+    -1px 1px 0 #131314,
+    1px 1px 0 #131314;
+`;
+
+export const ModalOverview = styled.p`
+  font-family: 'Roboto', sans-serif;
+  font-size: 1rem;
+  padding-top: 20px;
+`;
+
+export const CloseButton = styled.span`
+  display: inline-block;
+  height: 40px;
+  width: 40px;
+  font-size: 1.5rem;
+  font-weight: bold;
+  border-radius: 50%;
+  background-color: rgba(255, 192, 203, 0.8);
+  position: relative;
+  top: -340px;
+  left: 450px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;

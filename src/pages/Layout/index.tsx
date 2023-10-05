1b4b00f602ff1a17c5c617b0c0f0fc6e67d12447
Boyeon Ihn <boyeonihn@gmail.com>
2023-10-05 13:25:18 +0900

    fix: update find currentUrlType logic

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -4,21 +4,30 @@ import { AnimatePresence } from 'framer-motion';
 import { useMatch, useNavigate, useParams } from 'react-router-dom';
 import { Loading, Movie, MovieList } from '@/components';
 import { URL_ARRAY } from '@/const';
-import { IAPIResponse, IMovie } from '@/types';
+import { IAPIResponse, IMovie, getPopular } from '@/types';
 import { Wrapper, Overlay } from './styled';
-import { getUseMatch, getCurrentUrlType } from '@/utils';
+import { getUseMatch, getCurrentUrlType, isUrlMovie } from '@/utils';
 
 export const Layout = () => {
   const navigate = useNavigate();
   const [clickedMovie, setClickedMovie] = useState<IMovie | null>(null);
   const mainRef = useRef(null);
   const { id } = useParams();
+  const array = getUseMatch(useMatch, URL_ARRAY);
+  const url = array.find((it) => it !== null);
+  const currentUrlType = getCurrentUrlType(url);
 
   const { isLoading, data } = useQuery({
     queryKey: [`${currentUrlType.url}Data`],
     queryFn: async () => {
-      const data: IAPIResponse = await currentUrlType.api();
-      return data.results;
+      if (isUrlMovie(currentUrlType.url)) {
+        const data: IAPIResponse = await getPopular();
+        console.log(data);
+        return data.results;
+      } else {
+        const data: IAPIResponse = await currentUrlType.api();
+        return data.results;
+      }
     },
   });
 

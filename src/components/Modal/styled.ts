5536d308ea2cd76e5db7aa92752f2be3cbc42cb5
Boyeon Ihn <boyeonihn@gmail.com>
2023-10-07 19:24:50 +0900

    style: apply styling for Modal

--- src/components/Modal/styled.ts
+++ src/components/Modal/styled.ts
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 export const ModalCard = styled(motion.section)`
   position: fixed;
   width: 500px;
-  height: 80vh;
+  height: 85vh;
   background-color: pink;
   top: 50px; /* specify units for numerical values */
   left: 0;
@@ -19,11 +19,38 @@ export const ModalDetail = styled.div`
   font-size: 1rem;
   position: relative;
   top: -115px;
-  padding: 20px;
+  padding: 0px 20px;
 `;
 
 export const ModalTagline = styled.h3`
   font-size: 1.75rem;
   font-family: 'Pacifico', cursive;
   margin-bottom: 10px;
+`;
+
+export const List = styled.ul`
+  display: flex;
+`;
+
+export const Item = styled.li`
+  margin: 10px 10px 10px 0px;
+  background: #aaf0d1;
+  border-radius: 10px;
+  padding: 10px;
+
+  &:hover {
+    background: #47eda4;
+  }
+`;
+
+export const MovieLink = styled.a`
+  font-style: italic;
+
+  &:hover {
+    font-weight: bold;
+  }
+`;
+
+export const ModalP = styled.p`
+  padding-top: 3px;
 `;
